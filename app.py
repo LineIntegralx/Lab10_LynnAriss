7f03f6346f2f1be4edc66d6e3c34a23a661beda3
lynn Ariss <laa100@mail.aub.ed>
2025-11-05 13:47:02 +0200

    reset

--- app.py
+++
@@ -1,7 +0,0 @@
-﻿def greet(name):
-    return f"Hello, {name} from Lynn Ariss!"
-
-
-if __name__ == '__main__':
-
-    print(greet('World'))
--- tests/test_app.py
+++
@@ -1,9 +0,0 @@
-﻿import unittest
-from app import greet
-
-class TestApp(unittest.TestCase):
-    def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from Lynn Ariss!") 
-
-if __name__ == "__main__":
-    unittest.main()
